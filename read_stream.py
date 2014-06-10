9d7ba3f1057a3c8720a50595fe6cc4160be95556
John Gergely <john.gergely@continuum.io>
2014-06-09 22:48:10 -0400

    Initial commit - script to read data from MTA real-time stream API. Requires compiling google's protocol buffer

---
+++ read_stream.py
@@ -0,0 +1,60 @@
+import gtfs_realtime_pb2
+import urllib2
+import httplib
+
+# MTA Feed API Key
+STREAM_URL = """http://datamine.mta.info/mta_esi.php?key="""
+FEED_ID = """&feed_id=1"""
+# you need to obtain an API key from the MTA website and place it in this file in your local directory
+with open("my_api_key",'r') as f:
+    API_KEY = f.readline()[:-1]
+
+def readMTADataStream():
+    nyct_feed = gtfs_realtime_pb2.FeedMessage()
+    try:
+        stream = urllib2.urlopen(STREAM_URL + API_KEY + FEED_ID)
+        nyct_feed.ParseFromString(stream.read())
+        stream.close()
+        return nyct_feed
+    except urllib2.URLError:
+        print "Error: URLError - Failed to open URL."
+        return None
+    except httplib.IncompleteRead:
+        print "Error: IncompleteRead - Failed to parse stream."
+        return None
+    else:
+        print "************** Other failure ********************"
+        return None
+
+def processMTAData(nyct_feed, filename):
+    timestamp = nyct_feed.header.timestamp
+    with open(filename, 'a') as f:
+        count = 0
+        for entity in nyct_feed.entity:
+            if entity.trip_update.trip.trip_id:
+                stops = [stu for stu in entity.trip_update.stop_time_update]
+                if len(stops)>0:
+                    count += 1
+                    f.write(str(timestamp) + ',' + str(entity.trip_update.trip.trip_id) \
+                            + ',' + str(stops[0].stop_id) + '\n')
+        print "dump complete",count
+
+if __name__ == "__main__":
+    import time
+    max_time = 360000
+    local_time = 0
+    wait_time = 30
+    while(local_time < max_time):
+        print local_time,
+        attempts = 0
+        nyct_feed = None
+        while not nyct_feed:
+            attempts += 1
+            nyct_feed = readMTADataStream()
+        print "(read successful",attempts,")",
+        t = time.localtime()
+        filename = "mta_data_" + str(t[0]) + "." + str(t[1]) + "." + str(t[2]) + ".csv"
+        processMTAData(nyct_feed, filename)
+        time.sleep(wait_time)
+        local_time += wait_time
+    print "data collect finished."
