6d81f20adf7c936662865a0716f32efaf7f0160c
johngergely <johngergely@gmail.com>
2014-08-19 11:02:07 -0400

    Generate basic html to display link to plot

--- createLink.py
+++ createLink.py
@@ -16,8 +16,19 @@ def make_URL(SERVER_URL):
 
 def write_template(URL):
     with open("index.html",'w') as f:
-            f.write("<html>")
-            f.write("<body>")
-            f.write("<a href=URL>View real-time visualization.</a>")
-            f.write("</body>")
-            f.write("</html>")
+            f.write("<html>\n")
+	    f.write("<title>CONDUCTOR - Real-time NYC Subway Visualization and Analytics</title>\n")
+            f.write("<body>\n")
+
+	    f.write("<p>Conductor uses the New York City subway system's real-time data feed to do analysis and interactive visualization of the subways, incorporating live as well as historical data.</p>\n")
+
+            f.write("<p><a href=\"" + URL + "\">View the real-time visualization.</a></p>\n")
+
+	    f.write("<p>Conductor is written in Python. It is an open source project under active development. Here is the project on <a href=\"http://github.com/johngergely/conductor\">github</a>.</p>\n")
+
+            f.write("</body>\n")
+            f.write("</html>\n")
+
+if __name__=="__main__":
+    make_URL("http://104.131.255.76:5006/")
+    print "Updated index.html"
