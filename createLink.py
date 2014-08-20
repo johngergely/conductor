16e5b6b3b2756441b23e9f89462cdb5120d424b4
johngergely <johngergely@gmail.com>
2014-08-20 11:21:28 -0400

    edits to live plot homepage

--- createLink.py
+++ createLink.py
@@ -23,14 +23,12 @@ def write_template(embed_data):
             f.write(embed_data)
 
             f.write("<br>")
-            f.write("<br>")
 	    f.write("<h1>Using Conductor</h1>\n")
-            f.write("<p>The interactive plot should be displayed here. Refresh the page if it is not displaying correctly.</p>\n")
+            f.write("<p>The interactive plot should be displayed here. Refresh the page if it is not displayed, or if you are not seeing the visualization update every few seconds.</p>\n")
             f.write("<p>You can select interactive tools along the right side of the plot: pan, box zoom, resize, and reset view.</p>\n")
-            f.write("<p>This plot represents the real-time state of the 4/5/6 lines of the New York City subway system. By hovering your mouse over train and station objects, you can view data about the system, including geographical location, and trip duration and current status.</p>\n")
+            f.write("<p>This plot represents the real-time state of the 4/5/6 lines of the New York City subway system. By hovering over train and station objects, you can view data about the system, including geographical location, and trip duration and current status.</p>\n")
             f.write("<p>If you zoom in along the routes, you'll notice yellow shading along certain segments. This is a representation of how much trip times vary for the given segment. The width of the yelow-shaded area is proportional to the ratio of the median trip time along that semgent to the minimum trip time along that segment. In other words, for a segment of the trip with a wide shaded area, the time to cover this segment varies considerably and this segment is a source of delays.</p>\n")
 
-            f.write("<br>")
             f.write("<br>")
 	    f.write("<h1>About Conductor</h1>\n")
             f.write("<p>Conductor uses the New York City subway system's real-time data feed to do analysis and interactive visualization of the subways, incorporating live as well as historical data.</p>\n")
