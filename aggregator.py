9ea03a81b61fe6292740a409a4b6a2419694e3ad
johngergely <johngergely@gmail.com>
2014-10-08 16:56:55 -0400

    Store computed stop time interval data fetch if pre-computed

--- aggregator.py
+++ aggregator.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import time
+import pickle
 
 from systemData import routeData, stationLoc
 from visualize import file_timestamp
@@ -54,14 +55,25 @@ class aggregator():
 ########
 
 class intervalAggregator():
-        def __init__(self, ll, dd):
+        def __init__(self, ll, dd, useComputed=True):
+                self.line = str(ll)
+                self.direction = str(dd)
+                self.ID = "Agg_" + self.line + "_" + self.direction
+                self.useComputed = useComputed
+                self.firstCalc = True
+                self.loadData()
+                if not self.useComputed:
+                        print "useComputed flag is set to false.\nThis forces re-calculation of interval data and is a time-consuming call.\nThis call should only be made occasionally when re-computes are required. Otherwise re-use archived data if possible by setting useCompute=True."
+
+        def calcInit(self):
                 ## needs to be more robust access to database
-                fname_string = "data/subway_data_" + file_timestamp + "_" + ll + "_" + dd + "_stoptimes.csv"
+                fname_string = "data/subway_data_" + file_timestamp + "_" + self.line + "_" + self.direction + "_stoptimes.csv"
                 self.df = pd.read_csv(fname_string, index_col=0)
                 self._data_tmax = self.df.max().max() 
                 self._data_tmin = self.df.min().min()
                 self.df["date"] = self.df.index.map(lambda x: x.split("::")[1])
                 self.dates = self.df["date"].unique()
+                # quotient of 6000 converts from hundredths of a second to minutes
                 self.df["time"] = self.df.index.map(lambda x: int(x.split("_")[0])/6000.)
 
         def calcInterval(self, hmin, hmax, station):
@@ -79,11 +91,42 @@ class intervalAggregator():
                         intervals = ticks[1:] - ticks[:-1]
                         all_intervals = np.hstack((all_intervals, intervals))
                 station_mean = all_intervals.mean()/60.
+                if np.isnan(station_mean):
+                        return 0.
                 return station_mean
 
         def calcSeries(self,station):
+                if self.firstCalc:
+                        self.calcInit()
+                        self.firstCalc = False
                 means = {}
                 for hmin in range(0,24):
                         hmax = hmin+1
                         means[hmin] = self.calcInterval(hmin, hmax, station)
+                self.computedSeries[station] = means
+                #print station,"::",self.computedSeries[station]
                 return means
+      
+        def loadData(self):
+                try:
+                        with open("data/.intervalAggregatorArchive_" + self.line + "_" + self.direction + ".dict",'r') as f:
+                                self.computedSeries = pickle.load(f)
+                                print self.ID, "Loaded interval archive with",len(self.computedSeries.keys()),"station records"
+                except:
+                        self.computedSeries = {}
+                        print self.ID, "failed to load data. starting with empty dict"
+
+        def storeData(self):
+                if self.firstCalc:
+                        print "aggregator.storeData called but no new data computed; nothing to store"
+                else:
+                        with open("data/.intervalAggregatorArchive_" + self.line + "_" + self.direction + ".dict",'w') as f:
+                                pickle.dump(self.computedSeries, f)
+                        print "Wrote",len(self.computedSeries),"computed station records to archive"
+
+        def fetchSeries(self,station):
+                if self.useComputed and self.computedSeries.get(station):
+                        return self.computedSeries[station]
+                else:
+                        #print self.ID,"did not find records for station",station,self.computedSeries.keys()
+                        return self.calcSeries(station)
