b1a6e435cd76ded45fc066c5b4d3e5cd1d6989db
johngergely <johngergely@gmail.com>
2014-10-27 15:27:42 -0400

    using full archive of historical data. also now tabulated dicts are included in repo

--- aggregator.py
+++ aggregator.py
@@ -10,6 +10,47 @@ from stations import station_names
 
 TIME_RESOLUTION_FACTOR = 1e9
 
+# handle a sequence of stoptime.csv files that are concatenated together
+def concat(ll,dd):
+    with open("stoptimes/manifest." + ll + "_" + dd + ".txt",'r') as f:
+        manifest = f.read().split("\n")
+    DFs = []
+    for filename in manifest:
+        if filename!="":
+            print "importing>>",filename,"<<"
+            df = pd.read_csv("stoptimes/"+filename, index_col=0)
+            DFs.append(df)
+    DFconcat = pd.concat(DFs)
+    DFconcat.to_csv("stoptimes/master." + ll + "_" + dd + ".concat.csv")
+    return DFconcat
+
+def preprocess(fname):
+    raw=[]
+    dfList = []
+    with open(fname,'r') as f:
+        line = f.readline()
+        while line!="":
+            if line[:2] == "id":
+                dfList.append(raw)
+                raw = []
+            raw.append(line.split(","))
+            line = f.readline()
+    print "preparing",len(dfList),"dataframe objects from csv",fname
+
+    DFs = []
+    for data in dfList[1:]:
+        df = pd.DataFrame(data=data)
+        df.index = df[0]
+        df.columns = df.loc['id',:]
+        df = df.drop('id')
+        df = df.drop(['id'],axis=1)
+        DFs.append(df.astype(int))
+        print df.columns
+
+    DFconcat = pd.concat(DFs)
+    DFconcat.to_csv(fname[:-3] + "concat.csv")
+    return DFconcat
+
 NULL_STATS = {}
 for i in range(24):
         NULL_STATS[i] = {"min":240, "50%":240, "75%":240}
@@ -34,11 +75,17 @@ class aggregator():
 
         def calcInit(self):
                 ## needs to be more robust access to database
-                fname_string = "data/subway_data_" + file_timestamp + "_" + self.line + "_" + self.direction + "_stoptimes.csv"
-                self.df = pd.read_csv(fname_string, index_col=0)
+                #fname_string = "data/subway_data_" + file_timestamp + "_" + self.line + "_" + self.direction + "_stoptimes.csv"
+                fname_string = "stoptimes/master." + self.line + "_" + self.direction + ".concat.csv"
+                self.df = pd.read_csv(fname_string, index_col=0, error_bad_lines=False, warn_bad_lines=True)
+                print "DF len",len(self.df)
+                print "droping label rows",len(self.df[self.df.index=="id"])
+                self.df = self.df.drop(["id"])
+                #badI = self.df[self.df.index==np.nan].index
+                #print "bad index",len(badI)
                 self._data_tmax = self.df.max().max() 
                 self._data_tmin = self.df.min().min()
-                self.df["date"] = self.df.index.map(lambda x: x.split("::")[1])
+                self.df["date"] = self.df.index.map(lambda x: x.split("::")[-1])
                 self.dates = self.df["date"].unique()
                 # quotient of 6000 converts from hundredths of a minute to hours 
                 self.df["time"] = self.df.index.map(lambda x: int(x.split("_")[0])/6000.)
@@ -78,9 +125,13 @@ class aggregator():
                 if self.useComputed and self.computedSeries.get(token):
                         return self.computedSeries[token]
                 else:
-                        print self.ID," did not find records for",token
+                        if self.useComputed:
+                                print self.ID, "did not find records for",token
+                        else:
+                                print self.ID, "forcing compute for",token
                         return self.calcSeries(token)
 
+# average time between two stations
 class segmentAggregator(aggregator):
         def __init__(self, ll, dd, useComputed=True):
                 aggregator.__init__(self, ll, dd, useComputed)
@@ -106,7 +157,7 @@ class segmentAggregator(aggregator):
                 stats = diff_secs.describe()
                 return stats
 
-
+# frequency of trains at a given stop
 class frequencyAggregator(aggregator):
         def __init__(self, ll, dd, useComputed=True):
                 aggregator.__init__(self, ll, dd, useComputed)
@@ -118,13 +169,13 @@ class frequencyAggregator(aggregator):
                 df_select = self.df[self.df["time"]>hmin]
                 df_select = df_select[df_select["time"]<hmax]
                 if station not in df_select.columns:
-                        #print "NO DATA FOR STATION",station
+                        print self.ID,"NO DATA FOR STATION",station
                         return 0.
                 df_station = df_select[[station,"date"]]
                 all_intervals = np.array(())
                 for d in self.dates:
                         df_sub = df_station[df_station["date"]==d]
-                        ticks = df_sub[station].dropna().values
+                        ticks = df_sub[station].dropna().astype(int).values
                         ticks.sort
                         intervals = ticks[1:] - ticks[:-1]
                         all_intervals = np.hstack((all_intervals, intervals))
@@ -133,6 +184,7 @@ class frequencyAggregator(aggregator):
                         return 0.
                 return station_mean
 
+# trip duraton as a function of stop
 class durationAggregator(aggregator):
         def __init__(self, ll, dd, useComputed=True):
                 aggregator.__init__(self, ll, dd, useComputed)
@@ -149,14 +201,14 @@ class durationAggregator(aggregator):
 
         def calcInterval(self, hmin, hmax, station):
                 df_select = self.df[self.df["time"]>hmin]
-                df_select = df_select[df_select["time"]<hmax]
+                df_select = df_select[df_select["time"]<hmax].astype(float)
 	        if station not in df_select.columns:
                         return 0.
                 diff = df_select[station] - df_select['min']
                 # just drop negatives but should do something more sophisticated
                 todrop = diff[diff<0].index
                 if len(diff[diff<0]) > 0:
-                        print self.ID,"NEGATIVES",origin,dest,diff[diff<0].values
+                        print self.ID,"NEGATIVES",station,diff[diff<0].values
                 diff = diff.drop(todrop)
                 print self.ID,station,station_names[station],diff.mean()/60.
                 return diff.mean()/60.
